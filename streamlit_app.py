6f38cbc849ccdab67c575c8a0d44650c639eb1c4
siewmailomaigai <88498338+siewmailomaigai@users.noreply.github.com>
2024-09-29 01:26:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+from predict import run_prediction_page
+from tune import run_tuning_page
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Sidebar navigation
+st.sidebar.title("LightGBM Mental Health Prediction")
+page = st.sidebar.selectbox("Navigation", ["Prediction & LIME Analysis", "Model Tuning"])
+
+# Routing to different pages
+if page == "Prediction & LIME Analysis":
+    run_prediction_page()
+elif page == "Model Tuning":
+    run_tuning_page()
