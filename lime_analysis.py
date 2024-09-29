a79bdfa6709e2496f76cf529c5d0cfd3bd17116e
siewmailomaigai <88498338+siewmailomaigai@users.noreply.github.com>
2024-09-29 22:50:43 +0800

    Update lime_analysis.py

--- lime_analysis.py
+++ lime_analysis.py
@@ -1,17 +1,13 @@
-import streamlit as st
-from lime.lime_tabular import LimeTabularExplainer
-import matplotlib.pyplot as plt
+from model import X_train_smote, X_test
 
-# Function to display LIME Analysis
 def display_lime_analysis(model):
-    st.subheader("LIME Interpretability Analysis")
+    explainer = LimeTabularExplainer(X_train_smote.values, feature_names=X_train_smote.columns, class_names=['Low', 'High'], mode='classification')
 
-    # LIME analysis for interpretability
-    explainer = LimeTabularExplainer(X_train_smote.values, feature_names=X_train.columns, class_names=['Low', 'High'], mode='classification')
-
-    # Loop through a few instances and display LIME explanations
-    for instance_idx in [0, 5, 10, 15, 20]:
-        st.write(f"Explanation for instance {instance_idx}:")
+    for instance_idx in [0, 5, 10, 15, 20]:  # Adjust based on instances of interest
         exp = explainer.explain_instance(X_test.iloc[instance_idx].values, model.predict_proba, num_features=len(X_test.columns))
-        fig = exp.as_pyplot_figure()
-        st.pyplot(fig)
+
+        # Plot the LIME explanation for feature importance
+        plt.figure(figsize=(6, 8))
+        exp.as_pyplot_figure()
+        plt.tight_layout()
+        plt.show()
