b68e4c631dc334893aa5a3ee559f9dbf4271028c
siewmailomaigai <88498338+siewmailomaigai@users.noreply.github.com>
2024-09-30 04:09:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,28 @@ from tuning import run_tuning_page
 
 # Load image icons
 image_1 = "Sustainable_Development_Goal_03GoodHealth.svg"
-image_2 = "APUlogo.jpg" 
+image_2 = "APUlogo.jpg"
 
-# Add image icons at the top left
-col1, col2 = st.columns([0.5, 0.5])  # Create two columns
-with col1:
-    st.image(image_1, width=150)  
-
-with col2:
-    st.image(image_2, width=150)
+# Add image icons side by side
+st.markdown(
+    """
+    <style>
+    .container {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+    }
+    .container img {
+        margin-right: 20px; /* Adjust margin as necessary */
+    }
+    </style>
+    <div class="container">
+        <img src="Sustainable_Development_Goal_03GoodHealth.svg" alt="Image 1" width="150">
+        <img src="APUlogo.jpg" alt="Image 2" width="150">
+    </div>
+    """, 
+    unsafe_allow_html=True
+)
 
 # Sidebar navigation
 st.sidebar.title("Navigation")
