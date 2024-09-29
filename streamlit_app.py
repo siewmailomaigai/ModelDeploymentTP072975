1d4a0d3da4a9d92c670b04e159694c74b7bab5a5
siewmailomaigai <88498338+siewmailomaigai@users.noreply.github.com>
2024-09-30 03:41:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from tuning import run_tuning_page
 
 # Main App
-st.sidebar.title("Navigation")
+st.sidebar.title("|||")
 page = st.sidebar.radio("Tune", ["Model Tuning"])
 
 # Routing to the pages
